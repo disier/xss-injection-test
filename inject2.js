6d29235cc51722650bdb58077c82d2fca8234ca5
Dani Crespo <disier@gmail.com>
2022-03-26 21:12:56 +0100

    Create inject2.js

---
+++ inject2.js
@@ -0,0 +1,8 @@
+/**
+ * This file contain javascript code dinamically injected and executed on host page
+ */
+console.log("Code injected");
+
+
+$('body').append(
+    '<div class="modal" tabindex="-1" role="dialog"></div><div class="modal-dialog" role="document">  <div class="modal-content">    <div class="modal-header">      <h5 class="modal-title">Message</h5>      <button type="button" class="close" data-dismiss="modal" aria-label="Close">        <span aria-hidden="true">&times;</span>      </button>    </div>    <div class="modal-body">      <p>This modal window is raised by third party .JS file loaded outside CORS or CORB rules</p>    </div>    <div class="modal-footer">       <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>    </div>  </div></div></div>')
